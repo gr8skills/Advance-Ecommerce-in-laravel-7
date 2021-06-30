00cb416eeee4b36657c64f418709b4602f0ea503
tope <tope@winexviv.com>
2021-06-30 21:07:02 +0100

    owambe&Co

--- resources/views/backend/layouts/file-manager.blade.php
+++ resources/views/backend/layouts/file-manager.blade.php
@@ -4,3 +4,4 @@
         <iframe src="/laravel-filemanager" style="width: 100%; height: 500px; overflow: hidden; border: none;"></iframe>
     </div>
 @endsection
+<script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
